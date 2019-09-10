d26a273c909ec3fda28a2028112cbed36505eaba
Kat Moore <k.moore@nki.nl>
2019-09-10 13:00:10 +0200

    Examined whether removing batch effects with ComBat improves detection of molecular signature (it doesn't)

--- src/deseq_report_functions.R
+++ src/deseq_report_functions.R
@@ -435,3 +435,25 @@ blind_vst = function(dds){
   #mat = assay(vsd)
   return(vsd)
   }
+
+gene_report = function(list_reports, ensembl_id, pthresh = 0.05, abslogfcthresh = 0.5){
+
+  require(tidyverse)
+
+  #print(paste("Adjusted p value threshold:", pthresh, ", abs(log2FoldChange) threshold:", abslogfcthresh))
+
+  comp = lapply(list_reports,
+                function(x) x[x$ensembl_gene_id==ensembl_id,
+                              c("ensembl_gene_id","gene_name","log2FoldChange","padj")]) %>%
+    unlist() %>% enframe() %>% separate(name, into = c("comparison", "field"), sep="\\.") %>%
+    spread(key=field, value=value) %>%
+    mutate(log2FoldChange = as.numeric(log2FoldChange),
+           padj = as.numeric(padj),
+           padj_cutoff = !!pthresh,
+           absLog2FoldChange_cutoff = !!abslogfcthresh)
+
+  comp = comp %>% mutate(sig = (padj < !!pthresh) & (abs(log2FoldChange) > !!abslogfcthresh)) %>%
+    select(comparison, sig, padj, log2FoldChange, everything())
+
+  return(comp)
+}
---
+++ src/general_R_tools.R
@@ -0,0 +1,101 @@
+read_excel_tabs = function(path){
+  require(tidyverse)
+  tab_names = readxl::excel_sheets(path)
+  ldf = lapply(tab_names, function(x) readxl::read_excel(path, sheet = x))
+  names(ldf) = tab_names
+  return(ldf)
+}
+
+subset_tximport <- function(txi, cols){
+  require(tximport)
+  lapply(txi, function(x) if ( is.matrix(x) ) return(x[, cols]) else return(x))
+}
+
+unique_id_from_replicates = function(df, column_name){
+
+  require(tidyverse)
+
+  #Programming with dplyr the easy way: Standardize the column name
+  colnames(df)[colnames(df)==column_name] = "id"
+
+  #Columns for to track duplication and a dummy replicate column
+  df = df %>% mutate(dup = duplicated(id), rep=0) %>% select(id, dup, rep, everything())
+
+  #Add one to the previous counter if the id is duplicated
+  rep=c()
+  for (i in 1:nrow(df)){
+    if(df[i,"dup"]==T){
+      rep = c(rep, rep[[i-1]] + 1)
+    } else {
+      rep = c(rep, 1)
+    }
+  }
+  df$rep = rep
+
+  #Concatenate the rep counter with the id and ditch the extraneous columns
+  df= df %>% mutate(id = paste(id, rep, sep="_")) %>%
+    select(-dup, -rep) %>% select(id, everything())
+
+  return(df)
+}
+
+summarize_expression_duplicate_ids <- function(mat, id_column, f=colMeans, final_gene_symbol_colname="GeneSymbol"){
+  require(dplyr)
+
+  print(paste("Starting with gene expression matrix containing", nrow(mat), "rows."))
+
+  #Easiest way to write functions with dplyr is to standardize the column name
+
+  if(id_column != "symbol"){
+    colnames(mat)[colnames(mat)==id_column] <- "symbol"
+  }
+
+  #Make frequency table
+  id_table <- as.data.frame(table(mat$symbol))
+
+  #Identify duplicate genes
+  dups <- id_table$Var1[id_table$Freq > 1]
+  stopifnot(length(dups) == length(unique(dups)))
+  print(paste("Number of genes with duplicate names:", length(dups)))
+
+  #Set aside rows with unique gene names
+  nodup_df <- mat[!mat$symbol %in% dups,]
+
+  #Set aside rows with duplicate ids
+  dup_df <- mat[mat$symbol %in% dups,]
+  stopifnot(nrow(nodup_df) + nrow(dup_df) == nrow(mat))
+
+  #Sort by recurring id
+  dup_df <- dup_df[order(dup_df$symbol),]
+
+  print(paste("Number of rows with duplicate gene ids:", nrow(dup_df)))
+
+  #Mean expression fpkm of genes with the same symbol
+  mean_exps <- matrix(ncol = ncol(dup_df)-1, nrow=0) #Empty matrix, -1 gene symbol column
+
+  for (i in 1:length(unique(dup_df$symbol))){
+    #Subset rows with same symbol, discard symbol column, then apply aggregate function
+    exp <- f(as.matrix(dup_df[dup_df$symbol==unique(dup_df$symbol)[i], -1]))
+    mean_exps <- rbind(mean_exps, exp)
+  }
+  stopifnot(nrow(mean_exps) == length(unique(dup_df$symbol)))
+
+  rownames(mean_exps) <- unique(dup_df$symbol)
+  mean_exps <- as.data.frame(mean_exps) %>% rownames_to_column("symbol")
+
+  dedupped_df <- rbind(mean_exps, nodup_df)
+  dedupped_df <- dedupped_df[order(dedupped_df$symbol),]
+
+  stopifnot(length(unique(dedupped_df$symbol))==length(dedupped_df$symbol)) #All symbols should not be unique
+  stopifnot(nrow(mat) - #starting number
+              nrow(dup_df) + #rows with duplicate genes...
+              length(dups) == #...which condense down into this many unique genes...
+              nrow(dedupped_df)) #...should equal the number of rows in the final matrix
+
+  print(paste("Number of genes after applying", substitute(f),  "to duplicate ids:", nrow(dedupped_df)))
+
+  #For estimate, the column with identifiers HAS to be called GeneSymbol or EntrezGeneID
+  colnames(dedupped_df)[colnames(dedupped_df)=="symbol"] <- final_gene_symbol_colname
+
+  return(dedupped_df)
+}
