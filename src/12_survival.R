dd3bfef49a307315f2113f14f05571792db999cd
Kat Moore <k.moore@nki.nl>
2019-11-15 15:53:46 +0100

    Added penalized elastic Cox models for OS and DRS to notebook 12.

--- src/12_survival.R
+++ src/12_survival.R
@@ -20,6 +20,8 @@ library(ggpubr)
 
 overwrite <- FALSE
 
+set.seed(123)
+
 #### Set up data ####
 
 # Pre-process data data
@@ -100,6 +102,10 @@ stopifnot(identical(rownames(mat), metadata$sample_name))
 
 coxdata = cbind(metadata, mat)
 #head(coxdata)
+
+met_at_diag = colData(dds) %>% as.data.frame() %>%
+  filter(months_to_drs == 0) %>% select(sample_name, months_to_drs, metastasis_at_diagnosis) %>%
+  pull(sample_name)
 
 #### Cox regression ####
 
@@ -187,4 +193,124 @@ if(file.exists(file.path(resDir, "12_allgenes_univ_dr.Rds")) == F | overwrite ==
   print(end-start)
 
   saveRDS(univ_gene_models, file=file.path(resDir, "12_allgenes_univ_dr.Rds"))
+}
+
+#### Elastic Cox ####
+
+#A pentalized cox model to determine which groups of covariates are most predictive of survival.
+
+#We'll want two versions: One matrix with just the gene expression, and one with gene expression plus clinical covariates.
+covariates <- c("age_at_diagnosis", "year_of_diagnosis","stage","grade")
+enet.gene <- coxdata[,21:ncol(coxdata)] #Gene names start at position 21
+rownames(enet.gene) <- coxdata$sample_name
+enet.gene <- as.matrix(enet.gene)
+
+
+enet.clin <- coxdata[,c(which(colnames(coxdata) %in% covariates), 21:ncol(coxdata))]
+rownames(enet.clin) <- coxdata$sample_name
+enet.clin <- as.matrix(enet.clin)
+
+#Overall survival/OS
+os = data.frame(time = coxdata$months_overall_survival, event = coxdata$overall_survival)
+rownames(os) = coxdata$sample_name
+
+#os = as.matrix(os)
+os = Surv(os$time, os$event)
+
+#DRS
+#When looking at drs, time point 0 (metastatic at diagnosis) is not allowed
+drs = data.frame(time = coxdata$months_to_drs, event = coxdata$distant_recurrence)
+rownames(drs) = coxdata$sample_name
+drs = drs[!rownames(drs) %in% met_at_diag,]
+#Some samples have months_to_drs = 0
+
+drs = drs[drs$time!=0,]
+#drs = as.matrix(drs)
+drs = Surv(drs$time, drs$event)
+head(drs)
+
+enet.gene.drs = enet.gene[!(rownames(enet.gene) %in% met_at_diag),]
+enet.clin.drs = enet.clin[!(rownames(enet.clin) %in% met_at_diag),]
+
+nested.cox <- function(x,y,nfolds=10,nfolds_inner=10,
+                       s="lambda.min",
+                       alpha=0.5){
+
+  #Based on nested.cv from the TANDEM package
+
+  n = length(y)
+  foldid = ceiling(sample(1:n)/n * nfolds)
+
+  fitted_relative_risk = rep(NA, length(y))
+  #y_hat = rep(NA, length(y))
+
+  features = list()
+  fitplots = list()
+  results = list()
+
+  for (i in 1:nfolds) {
+    ind = foldid == i
+    x_train = x[!ind, ]
+    y_train = y[!ind]
+    x_test = x[ind, ]
+    n_i = length(y_train)
+    foldid_i = ceiling(sample(1:n_i)/n_i * nfolds_inner)
+    fit = glmnet::cv.glmnet(x_train, y_train, family = "cox", alpha = alpha,
+                            foldid = foldid_i)
+    fitplots[[length(fitplots) + 1]] = fit
+    features[[length(features) + 1]] = coef(fit, s=s)[,1] %>% enframe() %>% filter(value!=0 & name != "(Intercept)")
+    fitted_relative_risk[ind] = as.vector(glmnet::predict.cv.glmnet(fit,
+                                                                    newx = x_test, s = s, alpha=alpha, type="response"))
+
+    print(i)
+  }
+
+  results$features <- features
+  results$fitplots <- fitplots
+
+  return(results)
+}
+
+
+#Overall survival, genes only
+
+if(file.exists(file.path(resDir, "12_glm_os_gene.Rds")) == F | overwrite == T){
+  print("Elastic Cox for overall survival, genes only...")
+  start <- Sys.time()
+  glm.os <- nested.cox(x = enet.gene, y = os, s = "lambda.min", alpha = 0.5)
+  end <- Sys.time()
+  print(end-start)
+  saveRDS(glm.os, file.path(resDir,"12_glm_os_gene.Rds"))
+}
+
+#Overall survival, including clinical covariates
+
+if(file.exists(file.path(resDir, "12_glm_os_clin.Rds")) == F | overwrite == T){
+  print("Elastic Cox for overall survival, including clinical covariates...")
+  start <- Sys.time()
+  glm.os.clin <- nested.cox(x = enet.clin, y = os, s = "lambda.min", alpha = 0.5)
+  end <- Sys.time()
+  print(end-start)
+  saveRDS(glm.os.clin, file.path(resDir,"12_glm_os_clin.Rds"))
+}
+
+#DRS, genes only
+
+if(file.exists(file.path(resDir, "12_glm_drs_gene.Rds")) == F | overwrite == T){
+  print("Elastic Cox for distant recurrence (metastasis), genes only...")
+  start <- Sys.time()
+  glm.drs <- nested.cox(x = enet.gene.drs, y = drs, s = "lambda.min", alpha = 0.5)
+  end <- Sys.time()
+  print(end-start)
+  saveRDS(glm.drs, file.path(resDir,"12_glm_drs_gene.Rds"))
+}
+
+#DRS, including clinical covariates
+if(file.exists(file.path(resDir, "12_glm_drs_clin.Rds")) == F | overwrite == T){
+  print("Elastic Cox for distant recurrence, including clinical covariates...")
+  start <- Sys.time()
+  glm.drs.clin <- nested.cox(x = enet.clin.drs, y = drs, s = "lambda.min", alpha = 0.5)
+  end <- Sys.time()
+  print(end-start)
+  saveRDS(glm.drs.clin, file.path(resDir,"12_glm_drs_clin.Rds"))
 }
