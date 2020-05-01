3fa6472017aa2cd8718908ffdffbf425384974c3
Kat Moore <k.moore@nki.nl>
2020-05-01 20:28:50 +0200

    Changed the categories for time in involution and breastfeeding duration.

--- src/survival_colors.R
+++ src/survival_colors.R
@@ -1,9 +1,13 @@
 surv_colors = list(
   overall_survival = c("1" = "black", "0" = "white"),
-  distant_recurrence = c("1" = "darkblue", "0" = "white")
+  distant_recurrence = c("1" = "darkblue", "0" = "white"),
+  involution_duration = c("<= 6 months" = "#3538F2", "> 6 months" = "#FF831E"),
+  breastfeeding_duration = c("<= 1 month" = "#264653", "> 1 month" = "#E9C46A")
 )
+
 
 saveRDS(object = surv_colors,
         file = here::here("data/Rds/survival_colors.Rds"))
 
-#color_grid(surv_colors$distant_recurrence)
+#scales::show_col(surv_colors$involution_duration)
+#scales::show_col(surv_colors$breastfeeding_duration)
