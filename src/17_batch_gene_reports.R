931be2f39d70bd5aeca32c710795780db6a9861a
Kat Moore <k.moore@nki.nl>
2020-06-16 14:41:24 +0200

    More QOL improvements for genes with multiple ensembl IDs in notebook 17 template.

--- src/17_batch_gene_reports.R
+++ src/17_batch_gene_reports.R
@@ -4,10 +4,38 @@ library(tidyverse)
 
 rm(list = ls())
 
+#List of genes from which to generate reports
 genes_to_report = read_csv(
   here("reports", "genes_to_report")
 )
 
+#Allows naming report with both gene symbol and ensembl_id
+bx_annot <- readRDS(here("shinyApp","VisualizePPBCgene","data","app_gx_annot.Rds"))
+dds = readRDS(here("data/Rds/08_dds_ovr_inv_vs_rest.Rds"))
+genes_passing_filter = rownames(dds)
+
+gene_lookup <- function(gene, dict=bx_annot){
+  
+  if(gene %in% dict$gene_name){
+    gene_name = gene
+    ensembl_gene_id = unique(dict[dict$gene_name == gene_name,]$ensembl_gene_id)
+  } else if(gene %in% dict$ensembl_gene_id) {
+    ensembl_gene_id = gene
+    gene_name = unique(dict[dict$ensembl_gene_id == ensembl_gene_id,]$gene_name)
+  } else {
+    stop("Provided gene is not a valid ensembl id or gene name")
+  }
+  
+  uni=unique(dict[dict$ensembl_gene_id == ensembl_gene_id[1],]$uniprot_id)
+  ent=unique(dict[dict$ensembl_gene_id == ensembl_gene_id[1],]$entrez_id)
+  
+  list(gene_name=gene_name,
+       ensembl_gene_id = ensembl_gene_id,
+       uniprot_id=uni,
+       entrez_id=ent)
+}
+
+#Project directory and parameters
 projDir <- "/DATA/share/postpartumbc"
 Rmd <- file.path(projDir, "reports/17_gene_report_template.Rmd")
 stopifnot(file.exists(Rmd))
@@ -16,10 +44,24 @@ overwrite = F
 outdir=here("reports/gene_reports")
 dir.create(outdir, showWarnings = F)
 
+#Loop over every gene and create report
 for(gene in genes_to_report$Gene){
-  print(gene)
   
-  outfile=file.path(outdir, paste0(gene, "_report.html"))
+  #If provided input is ensembl ID, lookup the name of the gene
+  #Title will be gene name or gene name and ens ID, if ens ID is provided
+  #If ensembl ID is not provided and gene contains more than one,
+  #All matching ensembl IDs will be used in the report
+  if (gene %in% bx_annot$ensembl_gene_id){
+    gn <- gene_lookup(gene)$gene_name
+    ens <- gene
+    title=paste(gn, ens, sep=":")
+  } else {
+    title <- gene
+  }
+  
+  print(title)
+  
+  outfile=file.path(outdir, paste0(title, "_report.html"))
   
   if (!file.exists(outfile) | overwrite == T){
     rmarkdown::render(input =  Rmd,
