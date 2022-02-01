48fab2d0687fcc431d87e5324e0c61ab333f16eb
Kat Moore <k.moore@nki.nl>
2022-02-01 05:12:38 +0100

    Enable calculating density for aggregate groups (just CD27 for now).

--- src/spatial/model_density.R
+++ src/spatial/model_density.R
@@ -46,9 +46,61 @@ model_density <- function(objects) {
     mutate(density = if_else(n==0, 0, n / area)) # Edge case if there are no cells at all
 }
 
+#Some aggregate groups for density (currently just for CD27+/- B and T cells)
+agg_cells <- list(
+  "CD3+FoxP3-" = c("CD3+CD27+FoxP3-", "CD3+CD27-FoxP3-"),
+  "CD20+" = c("CD20+CD27+", "CD20+CD27-")
+)
+
+#Calculate density for a single aggregate group
+density_aggs <- function(density, aggs){
+  newgroup <- names(aggs)
+  stopifnot(length(newgroup) == 1)
+  oldgroups <- aggs[[1]]
+  stopifnot(length(oldgroups) > 1)
+  
+  # Handle an edge case in which a sample is present is only one panel
+  # (and not the one that contains the cell groups to be aggregated)
+  if(!all(sapply(oldgroups, function(x){x %in% unique(density$cell_type)}))){
+    return(density)
+  }
+  
+  df <- density %>%
+     dplyr::filter(cell_type %in% {{oldgroups}})
+  
+  df %>%
+    group_by(t_number, classifier_label, panel, batch, area) %>%
+    summarise(n = sum(n), .groups = "drop") %>%
+    mutate(density = if_else(n==0, 0, n / area), .after = n) %>%
+    mutate(cell_type = {{newgroup}}, .after = panel)
+}
+
+#Loop over all aggregate groups
+aggregate_densities <- function(density) {
+  #Avoid duplicating entries if one row is absent
+  res <- 0
+  lapply(names(agg_cells), function(x){
+    if(!all(sapply(agg_cells[x], function(y){y %in% unique(density$cell_type)}))){
+      paste(paste0(unlist(agg_cells[[x]]), collapse = " and "), "not found")
+    } else {
+      res <- lapply(names(agg_cells), function(groups){
+        density_aggs(density, aggs = agg_cells[groups])
+      }) %>%
+        #return aggregates
+        bind_rows() %>%
+        #recombine with input
+        bind_rows(density, .)  
+    }
+  })
+  
+  if(res == 0){res <- density}
+  res
+}
+
 if (sys.nframe() == 0) {
   args <- parse_args(commandArgs(T))
   objects <- readRDS(args$objects)
   density <- model_density(objects)
-  write_tsv(density, args$out)
+  all_densities <- aggregate_densities(density)
+  write_tsv(all_densities, args$out)
 }
