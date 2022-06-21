74bb39ee482410677ffef4ccea7cadc5f822150d
Tycho Bismeijer <t.bismeijer@nki.nl>
2022-06-21 17:53:09 +0200

    Add Lcross to panck

--- src/spatial/model_lcross_panck.R
+++ src/spatial/model_lcross_panck.R
@@ -10,19 +10,26 @@ options(warn = 2)
 pixel_size <- 0.0005 #mm
 unitname <- c('milimeter', 'milimeters')
 
-compute_L <- function(cells, radii, ...) {
-  spatstat.core::Lest(cells, r=radii, correction='none') %>%
-    as_tibble() %>%
+
+compute_Lcross <- function(cells, radii, cell_type1, cell_type2, ...) {
+  if (sum(spatstat.geom::marks(cells) == cell_type2) == 0) {
+    tibble(estimate = NA)
+  } else {
+    spatstat.core::Lcross(cells, cell_type1, cell_type2, r=radii, correction='none') %>%
+    as_tibble()
+  } %>%
     transmute(
-      measure = 'L',
+      cell_type1 = cell_type1,
+      cell_type2 = cell_type2,
+      measure = 'Lcross',
       radius = r,
       estimate = un)
 }
 
-model_L <- function(cells, progress_bars, nsim=100) {
+model_Lcross <- function(cells, progress_bars, nsim=100) {
   cell_types <- levels(cells$marks)
   immune_cell_types <- setdiff(cell_types, c('PanCK+', 'Other'))
-
+  
   if (progress_bars) {
     n_tick_progress = (nsim + 1) * length(immune_cell_types)
     progress_bar <- progress::progress_bar$new(
@@ -34,15 +41,16 @@ model_L <- function(cells, progress_bars, nsim=100) {
 
   # Maximum radius to compute this statistic for is limited by the largest distance in our image
   # and what we are interested in.
-  radius_max <- min(spatstat.geom::inradius(spatstat.geom::as.owin(cells)), 1.0)
+  radius_max <- min(spatstat.geom::inradius(spatstat.geom::as.owin(cells)), 0.1)
   # Set the radii to compute, such they can later be aligned over multiple samples.
   radius_step <- 0.001
   radii <- seq(0, radius_max, radius_step)
   purrr::map_dfr(rlang::set_names(immune_cell_types), function (ct) {
-      cells <- subset(cells, marks == ct)
-      with_simulation(cells, nsim, compute_L, simple_poisson_null, radii = radii,
+      cells <- subset(cells, marks %in% c(ct, 'PanCK+'))
+      with_simulation(cells, nsim, compute_Lcross, subset_poisson_null, radii = radii,
+                      cell_type1 = 'PanCK+', cell_type2 = ct, resample_mark = ct,
                       .progress_bar = progress_bar)
-    }, .id = 'cell_type')
+      })
 }
 
 if (sys.nframe() == 0) {
@@ -58,6 +66,6 @@ if (sys.nframe() == 0) {
   args <- parse_args(commandArgs(T))
 
   cells <- read_cells(args$objects, args$annotation)
-  res <- model_L(cells, args$progress_bars)
+  res <- model_Lcross(cells, args$progress_bars)
   readr::write_tsv(res, args$output)
 }
