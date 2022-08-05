d8e7303569be71cbe85eef9a2d71d1a6bc88a105
Kat Moore <k.moore@nki.nl>
2022-08-05 15:47:16 +0200

    Remove some unused scripts

--- src/00-checksum.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-#for file in /DATA/share/postpartbc/fastq_jun2019/*/runs/*/v*/result/*R1.fastq.gz; do echo $file; done
-
-for file in /DATA/share/postpartbc/fastq_jun2019/*/runs/*/v*/result/*R1.fastq.gz; do echo "$file"; md5sum $file >> /DATA/share/postpartumbc/data/metadata/md5_jun2019.txt; done
--- src/bash_utilities.sh
+++
@@ -1,6 +0,0 @@
-#Return file name (and the path to the file) for every file of type .php|.html|.js 
-#containing the case-insensitive string "document.cookie" | "setcookie"
-#r = recursive
-#i = case-insensitive
-
-egrep -ir --include=*.{php,html,js} "(document.cookie|setcookie)"
